59943674a4247568ef5651d551013e4bcbcfaeef
Dara LY <dara.ly@ensta.fr>
2016-08-09 12:57:24 +0900

    add test module

---
+++ testsuite/tests/typing-dim/test.ml
@@ -0,0 +1,17 @@
+module Dim : sig
+    type 'a dfloat = private float
+    val create : float ->  dfloat [`a]
+    val (+:) : dfloat [`a] -> dfloat [`a] -> dfloat [`a]
+    val ( *: ) : dfloat [`a] ->  dfloat [`b] -> dfloat [`a * `b]
+    val ( /: ) : dfloat [`a] ->  dfloat [`b] -> dfloat [`a / `b]
+    val inv :  dfloat [`a] -> dfloat [1 / `a]
+  end = struct
+
+    type 'a dfloat = float ;;
+
+    let create f = f
+    let ( +: ) = ( +. )
+    let ( *: ) = ( *. )
+    let ( /: ) = ( /. )
+    let inv f = 1. /. f
+  end
