6662cfab78994abce21c002bef8f0bb4fce68ff3
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2016-10-13 15:24:00 +0900

    use proper syntax for units

--- testsuite/tests/typing-dim/test.ml
+++ testsuite/tests/typing-dim/test.ml
@@ -1,11 +1,11 @@
 module Dim : sig
   type 'a dfloat = private float
-  val create : float ->  dfloat [`a]
-  val (+:) : dfloat [`a] -> dfloat [`a] -> dfloat [`a]
-  val ( *: ) : dfloat [`a] ->  dfloat [`b] -> dfloat [`a * `b]
-  val ( /: ) : dfloat [`a] ->  dfloat [`b] -> dfloat [`a / `b]
-  val inv :  dfloat [`a] -> dfloat [1 / `a]
-  val dsqrt : dfloat [`a ** 2] -> dfloat [`a]
+  val create : float ->  dfloat ['a]
+  val (+:) : dfloat ['a] -> dfloat ['a] -> dfloat ['a]
+  val ( *: ) : dfloat ['a] ->  dfloat ['b] -> dfloat ['a * 'b]
+  val ( /: ) : dfloat ['a] ->  dfloat ['b] -> dfloat ['a / 'b]
+  val inv :  dfloat ['a] -> dfloat [1 / 'a]
+  val dsqrt : dfloat ['a ^ 2] -> dfloat ['a]
 end = struct
 
   type 'a dfloat = float
@@ -25,19 +25,19 @@ let x : dfloat [m] = create 3.;;
 let y : dfloat [m] = create 4.;;
 let _ = (x = y);;
 
-let div : dfloat [`a] ->  dfloat [`a * `b] -> dfloat [1 / `b] = ( /: );;
-let lcm (x: dfloat [`a]) (y: dfloat [`b]) (z: dfloat [`c]) =
+let div : dfloat ['a] ->  dfloat ['a * 'b] -> dfloat [1 / 'b] = ( /: );;
+let lcm (x: dfloat ['a]) (y: dfloat ['b]) (z: dfloat ['c]) =
   (x *: x) +: (y *: y *: y) +: (z *: z *: z *: z *: z);;
 let lcm_bis x y z =
   (x *: x) +: (y *: y *: y) +: (z *: z *: z *: z *: z);;
 
 (* polymorphic recursion *)
-let rec prodlist: 'a 'b. dfloat[`a] list -> dfloat[`b] list -> dfloat[`a * `b] list =
+let rec prodlist: 'a 'b. dfloat['a] list -> dfloat['b] list -> dfloat['a * 'b] list =
   fun  x y -> match x,y with
   | [],_ | _, [] -> []
   | (x::xs,y::ys) -> (x *: y) :: (prodlist ys xs);;
 
-let f (x : 'a option) : dfloat[`a] option = None;;
+let f (x : 'a option) : dfloat['a] option = None;;
 f (Some 1);; 
 
 module M : sig val v : dfloat [m] end =
